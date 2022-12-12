ea3eb5200b1fe4d7e7bec85bebbd2523e679a7a3
GerardoMorenoDW <gdmg507@gmail.com>
2022-12-12 02:08:03 -0500

    implementacion del Contador de pasos

--- app/src/main/java/com/example/proyectosensores/Pasos.java
+++ app/src/main/java/com/example/proyectosensores/Pasos.java
@@ -1,14 +1,78 @@
 package com.example.proyectosensores;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class Pasos extends AppCompatActivity {
+public class Pasos extends AppCompatActivity implements SensorEventListener{
+
+    private TextView textViewStepCounter, textViewStepDetector;
+    private SensorManager sensorManager;
+    private Sensor mStepcounter;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasos);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStepCounter = findViewById(R.id.textView5);
+        textViewStepDetector = findViewById(R.id.textView6);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+
+            mStepcounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        }else{
+            textViewStepCounter.setText("Sensor de contador no esta presente");
+            isCounterSensorPresent = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == mStepcounter) {
+            stepCount = (int) sensorEvent.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null){
+            sensorManager.registerListener(this, mStepcounter, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
+                sensorManager.unregisterListener(this, mStepcounter);
+        }
     }
 }
